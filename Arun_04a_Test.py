1b6837680f4b5d186e6aac870835f094c105c5dd
RuchaCB <36187469+RuchaCB@users.noreply.github.com>
2020-02-25 23:50:53 -0500

    Update Arun_04a_Test.py

--- Arun_04a_Test.py
+++ Arun_04a_Test.py
@@ -1,14 +1,15 @@
 import unittest
-from Arun_04a import getInfo
+import Arun_04a
 from unittest import mock
-
 
 class TestGit(unittest.TestCase):
     """Class is used to test getInfo"""
-    @mock.patch("urllib.request.urlopen")
+    @mock.patch("Arun_04a.getInfo")
     def test_getInfo(self, mock_get):
-        self.assertEqual([repo for repo in getInfo("richkempinski")], ([('hellogitworld', 30), ('helloworld', 6), ('Mocks', 10), ('Project1', 2), ('threads-of-life', 1)]))
-
+        mock_get.return_value = [('hellogitworld', 30), ('helloworld', 6), ('Mocks', 10), ('Project1', 2), ('threads-of-life', 1)]
+        expected = [('hellogitworld', 30), ('helloworld', 6), ('Mocks', 10), ('Project1', 2), ('threads-of-life', 1)]
+        git_id = "richkempinski"
+        self.assertEqual(expected, Arun_04a.getInfo(git_id))
 
 if __name__ == '__main__':
     unittest.main(exit=False, verbosity=2)
